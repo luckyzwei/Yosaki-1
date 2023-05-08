6554c8cd03c773c1ffba704dd414d7e1848faafc
DESKTOP-53VLV6K\wer37 <wer3799@naver.com>
2023-05-08 21:40:52 +0900

    no message

---
+++ Assets/02.Script/UserInfoTable2.cs
@@ -0,0 +1,171 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BackEnd;
+using LitJson;
+using System;
+using UniRx;
+using CodeStage.AntiCheat.ObscuredTypes;
+
+public class UserInfoTable2
+{
+    public static string Indate;
+    public const string tableName = "UserInfo2";
+    
+    public const string GangChulReset = "GangChulReset";
+    public const string levelPassFree = "levelPassFree";
+    public const string levelPassAd = "levelPassAd";
+
+    private Dictionary<string, double> tableSchema = new Dictionary<string, double>()
+    {
+        { GangChulReset, 0f },
+        { levelPassFree, -1f },
+        { levelPassAd, -1f },
+    };
+
+    private Dictionary<string, ReactiveProperty<double>> tableDatas = new Dictionary<string, ReactiveProperty<double>>();
+    public Dictionary<string, ReactiveProperty<double>> TableDatas => tableDatas;
+
+    public ReactiveProperty<double> GetTableData(string key)
+    {
+        return tableDatas[key];
+    }
+
+    public void Initialize()
+    {
+        tableDatas.Clear();
+
+        SendQueue.Enqueue(Backend.GameData.GetMyData, tableName, new Where(), callback =>
+        {
+            // 이후 처리
+            if (callback.IsSuccess() == false)
+            {
+                Debug.LogError("LoadStatusFailed");
+                PopupManager.Instance.ShowConfirmPopup(CommonString.Notice, CommonString.DataLoadFailedRetry, Initialize);
+                return;
+            }
+
+            var rows = callback.Rows();
+
+            //맨처음 초기화(캐릭터생성)
+            if (rows.Count <= 0)
+            {
+                Param defultValues = new Param();
+
+                var e = tableSchema.GetEnumerator();
+
+                while (e.MoveNext())
+                {
+                    defultValues.Add(e.Current.Key, e.Current.Value);
+                    tableDatas.Add(e.Current.Key, new ReactiveProperty<double>(e.Current.Value));
+                }
+
+                var bro = Backend.GameData.Insert(tableName, defultValues);
+
+                if (bro.IsSuccess() == false)
+                {
+                    // 이후 처리
+                    ServerData.ShowCommonErrorPopup(bro, Initialize);
+                    return;
+                }
+                else
+                {
+                    var jsonData = bro.GetReturnValuetoJSON();
+                    if (jsonData.Keys.Count > 0)
+                    {
+                        Indate = jsonData[0].ToString();
+                    }
+
+                    // data.
+                    // statusIndate = data[DatabaseManager.inDate_str][DatabaseManager.format_string].ToString();
+                }
+
+                return;
+            }
+            //나중에 칼럼 추가됐을때 업데이트
+            else
+            {
+                Param defultValues = new Param();
+                int paramCount = 0;
+
+                JsonData data = rows[0];
+
+                if (data.Keys.Contains(ServerData.inDate_str))
+                {
+                    Indate = data[ServerData.inDate_str][ServerData.format_string].ToString();
+                }
+
+                var e = tableSchema.GetEnumerator();
+
+                for (int i = 0; i < data.Keys.Count; i++)
+                {
+                    while (e.MoveNext())
+                    {
+                        if (data.Keys.Contains(e.Current.Key))
+                        {
+                            //값로드
+                            var value = data[e.Current.Key][ServerData.format_Number].ToString();
+                            tableDatas.Add(e.Current.Key, new ReactiveProperty<double>(double.Parse(value)));
+                        }
+                        else
+                        {
+                            defultValues.Add(e.Current.Key, e.Current.Value);
+                            tableDatas.Add(e.Current.Key, new ReactiveProperty<double>(e.Current.Value));
+
+                            paramCount++;
+                        }
+                    }
+                }
+
+                if (paramCount != 0)
+                {
+                    var bro = Backend.GameData.Update(tableName, Indate, defultValues);
+
+                    if (bro.IsSuccess() == false)
+                    {
+                        ServerData.ShowCommonErrorPopup(bro, Initialize);
+                        return; //
+                    }
+                }
+            }
+        });
+    }
+
+    public void UpData(string key, bool LocalOnly)
+    {
+        if (tableDatas.ContainsKey(key) == false)
+        {
+            Debug.Log($"UserInfoTable {key} is not exist");
+            return;
+        }
+
+        UpData(key, tableDatas[key].Value, LocalOnly);
+    }
+
+    public void UpData(string key, double data, bool LocalOnly, Action failCallBack = null)
+    {
+        if (tableDatas.ContainsKey(key) == false)
+        {
+            Debug.Log($"UserInfoTable {key} is not exist");
+            return;
+        }
+
+        tableDatas[key].Value = data;
+
+        if (LocalOnly == false)
+        {
+            Param param = new Param();
+            param.Add(key, tableDatas[key].Value);
+
+            SendQueue.Enqueue(Backend.GameData.Update, tableName, Indate, param, e =>
+            {
+                if (e.IsSuccess() == false)
+                {
+                    failCallBack?.Invoke();
+                    Debug.LogError($"UserInfoTable {key} up failed");
+                    return;
+                }
+            });
+        }
+    }
+}
